f4aab03c85bd33ddd3a1f6e52a3dc1b6ea660a46
AtQ0 <mail@atkobabic.com>
2026-01-30 14:06:51 +0100

    Make SIGNUP_SET_PASSWORD enum migration idempotent

--- prisma/migrations/20260130120708_make_userbook_status_nullable/migration.sql
+++ prisma/migrations/20260130120708_make_userbook_status_nullable/migration.sql
@@ -1,5 +1,17 @@
 -- AlterEnum
-ALTER TYPE "VerificationPurpose" ADD VALUE 'SIGNUP_SET_PASSWORD';
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_type t
+    JOIN pg_enum e ON t.oid = e.enumtypid
+    WHERE t.typname = 'VerificationPurpose'
+      AND e.enumlabel = 'SIGNUP_SET_PASSWORD'
+  ) THEN
+    ALTER TYPE "VerificationPurpose" ADD VALUE 'SIGNUP_SET_PASSWORD';
+  END IF;
+END
+$$;
 
 -- AlterTable
 ALTER TABLE "UserBook" ALTER COLUMN "status" DROP NOT NULL;
